5c8342b8c59b7284c7b50f86ee8857e13e48108e
Ivan3342 <stojadinovicivan0@gmail.com>
2026-08-31 14:28:15 -0400

    simplified this version for now

--- text_editor.c
+++ text_editor.c
@@ -2,21 +2,35 @@
 
 int main() {
 
+    FILE *f;
+
     int inp;
+    char[] inputString;
+
+    fopen("text.txt", "r+");
 
     while(true) {
         printf("Enter the action ID: ");
         printf("ID | Name  | Description\n");
-        printf(" 1 | New   | Create a new file\n");
-        printf(" 2 | Open  | Opens an already existing file\n");
-        printf(" 3 | Read  | Prints the contents of a selected file\n");
-        printf(" 4 | File  | Prints the name of the selected file\n");
-        printf(" 5 | Save  | Save the contents of the file\n");
+        printf(" 1 | Write | Write into a file\n");
+        printf(" 2 | Read  | Prints the contents of a selected file\n");
         printf("-1 | Close | Closes the program\n");
         scanf("%d", &inp);
-        if(inp == -1) {
-            printf("Closed the program.");
-            break;
+
+        switch(inp) {
+            case -1:
+                printf("Closed the program.\n");
+                return 0;
+                break;
+            case 1:
+                printf("Create a new file\n");
+                break;
+            case 2:
+                printf("Open a file\n");
+                break;
+            default:
+                printf("Wrong input, please select one of the following:\n");
+                break;
         }
     }
 
