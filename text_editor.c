2a65e33c6a36bc6c710cca4e1cf6e8486dca3a11
Ivan3342 <stojadinovicivan0@gmail.com>
2026-08-31 14:58:00 -0400

    added writing logic

--- text_editor.c
+++ text_editor.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void writef(FILE *f) {
+
+    char buf[1024];
+
+    f = fopen("text.txt", "a+");
+    printf("Enter the text you want to write:\n");
+
+    fgets(buf, sizeof(buf), stdin);
+    printf("%s",  buf);
+    fputs(buf, f);
+    printf("Writing complete.\n");
+
+    fclose(f);
+}
+
+void readf(FILE *f) {
+    return 0;
+}
 
 int main() {
 
     FILE *f;
 
     int inp;
-    char[] inputString;
 
-    fopen("text.txt", "r+");
-
-    while(true) {
-        printf("Enter the action ID: ");
+    while(1) {
+        printf("\nEnter the action ID:\n");
         printf("ID | Name  | Description\n");
         printf(" 1 | Write | Write into a file\n");
         printf(" 2 | Read  | Prints the contents of a selected file\n");
@@ -23,16 +41,18 @@ int main() {
                 return 0;
                 break;
             case 1:
-                printf("Create a new file\n");
+                writef(f);
                 break;
             case 2:
-                printf("Open a file\n");
+                readf(f);
                 break;
             default:
                 printf("Wrong input, please select one of the following:\n");
                 break;
         }
     }
+
+    writef(f);
 
     return 0;
 }
