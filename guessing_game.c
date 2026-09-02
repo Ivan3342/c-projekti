65e2dfdd858043ddfb1ce77e099c678012256ce0
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2026-09-02 21:28:46 +0200

    created the loop

--- guessing_game.c
+++ guessing_game.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()  {
 
     int guess_number = 10;
     int guess;
-    int number = rand()
+    srand(time(0));
+    int number = rand()%100;
+
+    do {
+        printf("Enter a number: ");
+        scanf("%d", &guess);
+
+        guess_number--;
+    }
+    while (guess_number > 0);
 
     return 0;
 }
