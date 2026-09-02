79f4f3abd6590d20e57736355192d9f91021fb63
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2026-09-02 21:33:57 +0200

    finished base game

--- guessing_game.c
+++ guessing_game.c
@@ -8,10 +8,24 @@ int main()  {
     int guess;
     srand(time(0));
     int number = rand()%100;
+    
+    printf("Debug: %d", number);
 
     do {
         printf("Enter a number: ");
         scanf("%d", &guess);
+
+        if(guess > number) {
+            printf("Lower...\n");
+        }
+        else if (guess < number) {
+            printf("Higher...\n");
+        }
+        else {
+            printf("You guessed the number, good job! The number was %d\n", number);
+            printf("It took you %d tries to guess the number.\n", (10-guess_number));
+            return 0;
+        }
 
         guess_number--;
     }
